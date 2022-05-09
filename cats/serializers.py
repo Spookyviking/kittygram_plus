87c2b056f141696fb6127d8bec370e9a2669d6ca
Maksim Ostapenko <spookyvikingooth@gmail.com>
2022-05-10 00:48:10 +0300

    Доработка API + попытка сделать аутентификацию по токеку

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,23 +1,55 @@
+import datetime as dt
+import webcolors
+
 from rest_framework import serializers
 
-from .models import Cat, Owner, Achievement, AchievementCat
+from .models import Cat, Owner, Achievement, AchievementCat, CHOICES
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    # переопределяем имя. Было 'name', стало 'achievement_name'
+    achievement_name = serializers.CharField(source='name')
 
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achievement_name') 
+
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
 
 
 class CatSerializer(serializers.ModelSerializer):
-    achievements = AchievementSerializer(many=True, required=False)  # Убрали read_only=True
+    achievements = AchievementSerializer(many=True)  # Убрали read_only=True и required=False
     # required=False указывает на необязательность поля
     # Убрали owner = serializers.StringRelatedField(read_only=True)
+    age = serializers.SerializerMethodField()
+    # содержимое этого поля будет вычисляться «на лету» в методе get_age
+
+    # color = Hex2NameColor()
+    # Теперь поле примет только значение, упомянутое в списке CHOICES
+    color = serializers.ChoiceField(choices=CHOICES)
     
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements',
+                  'age')
+    
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
     
     # def create(self, validated_data):
     #     # Уберем список достижений из словаря validated_data и сохраним его
--- kittygram_plus/urls.py
+++ kittygram_plus/urls.py
@@ -1,4 +1,5 @@
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views
 
 from django.urls import include, path
 
@@ -10,5 +11,14 @@ router.register('cats', CatViewSet)
 router.register('owners', OwnerViewSet)
 
 urlpatterns = [
+    #path('api-token-auth/', views.obtain_auth_token),
+
+    # Djoser создаст набор необходимых эндпоинтов.
+    # базовые, для управления пользователями в Django:
+    path('auth/', include('djoser.urls')),
+
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('auth/', include('djoser.urls.jwt')),
+    
     path('', include(router.urls)),
 ] 
